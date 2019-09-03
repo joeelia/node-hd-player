4e145b1458c50fea5257cae6f74a9adfee2574ee
Joe Elia <44442621+joeelia@users.noreply.github.com>
2019-09-03 18:46:58 -0400

    Delete node.lua

--- save_proof_of_play/node.lua
+++
@@ -1,7 +0,0 @@
-save_proof_of_play{
-     asset_id = asset_id, -- What are we playing
-     duration = duration, -- .. for how long
-     start = os.time(), -- unix timestamp of when playback started
-     device = sys.getenv "SERIAL", -- the serial number of the device
-     uuid = generate_some_uuid(), -- see below
-}
