a4ab9b2206c90fb80ab8eb6396443e502bc397ff
Joe Elia <44442621+joeelia@users.noreply.github.com>
2019-09-03 16:00:54 -0400

    Update node.lua

--- node.lua
+++ node.lua
@@ -243,7 +243,7 @@ local Scheduler = (function()
         local item
         item, playlist_offset = cycled(playlist, playlist_offset)
         print(string.format("next scheduled item is %s [%f]", item.asset_name, item.duration))
-        print(item.asset_name)
+        print(item.asset_id)
         return item
     end
 
